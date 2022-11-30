7ca342890f359391ee4b5ba664aa80413655935d
Daniel Hernández Fajardo <alu0101320489@ull.edu.es>
2022-11-30 21:03:37 +0000

    stopwords y lematizacion

--- operaciones.js
+++ operaciones.js
@@ -18,20 +18,43 @@ document.getElementById("stopwords")
               };  
           });
 
+document.getElementById("corpus")
+            .addEventListener("change", function () {
+              var fr = new FileReader();
+              fr.readAsText(this.files[0]);
+              fr.onload = function () {
+                ficheros[2] = fr.result;
+              };  
+          });
+
 
 function main() {
+    //Formatea los documentos
     ficheros[0]=ficheros[0].replaceAll('.', '');
     ficheros[0]=ficheros[0].replaceAll(',', '');
     ficheros[0]=ficheros[0].toLowerCase();
-    const docs = ficheros[0].split("\n")
-    const stopwords = ficheros[1].split("\n")
-    //console.log(docs)
+    ficheros[0]=ficheros[0].replaceAll('\n', '');
+    const docs = ficheros[0].split("\r")
+    //Formatea las stopwords
+    ficheros[1]=ficheros[1].replaceAll('\n', '');
+    const stopwords = ficheros[1].split("\r")
+    
+    ficheros[2]=JSON.parse(ficheros[2])
+
     var terminos = docs[0].split(" ")
 
-    
-    terminos.forEach(element => console.log(stopwords.indexOf(element.toString()))
-        //terminos.splice(terminos.indexOf(element),1)
-    )
-
+    //Elimina las stopwords
+    for (let i = 0; i < terminos.length; i++) {
+      if (stopwords.includes(terminos[i])) {
+        terminos.splice(i,1)
+      }
+    }
+    console.log(terminos)
+    //Sustituye los terminos para la lematización
+    for (let i = 0; i < terminos.length; i++) {
+      if (ficheros[2][terminos[i]] != undefined) {
+        terminos[i] = ficheros[2][terminos[i]];
+      }
+    }
     console.log(terminos)
 }
